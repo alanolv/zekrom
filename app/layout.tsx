317d396e656f2ed19efed243649fd478d4fbc867
Alan Olvera <olvera.cristino.alan@gmail.com>
2026-01-22 13:15:05 -0600

    feat:hero finished and primary css variable configured

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <main className="min-h-screen">{children}</main>
+        <main className="min-h-screen bg-gray-75">{children}</main>
         <Footer />
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Header from "../components/landing/header";
+import Hero from "../components/landing/Hero";
 
 export default function Home() {
-  return <section></section>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
